934fed1425599020fe0250ba2e527b35776b9ce0
léo <btssioleo.espeu@gmail.com>
2017-07-25 19:01:05 +0200

    modification intermediaire

--- winformemploye/winformemploye/Form1.cs
+++ winformemploye/winformemploye/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
 
 namespace winformemploye
 {
     public partial class Form1 : Form
     {
+        internal OracleConnection oracleCnx;
         public Form1()
         {
             InitializeComponent();
@@ -29,7 +31,13 @@ namespace winformemploye
 
         private void consulterSéminaireToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            string cnx = @"DATA SOURCE=(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = freesio.lyc-bonaparte.fr)(PORT = 21521))(CONNECT_DATA = (SERVICE_NAME = slam)));User Id= " + textBox1.Text + ";Password=" + textBox3.Text;
+            oracleCnx = new OracleConnection(cnx);
+            oracleCnx.Open();
+            string requete = "";
+            consulterSéminaire consulteSem = new consulterSéminaire();
+            consulteSem.Owner = this;
+            consulteSem.ShowDialog(this);
         }
 
         private void consulterEmployéToolStripMenuItem_Click(object sender, EventArgs e)
